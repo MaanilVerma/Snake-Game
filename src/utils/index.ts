7f0ff209bad566f0058cd381f5c2e56e018a2bfc
Maanil Verma <vermaMaanil97@gmail.com>
2023-01-20 08:52:42 +0530

    feat: Add more utility functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -60,3 +60,35 @@ export const getSnakeCell = (
   snakeCell.isSnakeBodyPart = true;
   return snakeCell;
 };
+
+export const generateMazeWall = (grid: CellInterface[][]) => {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      let currentCell = grid[i][j];
+      if (
+        !currentCell.isSnakeBodyPart &&
+        (i === 0 || j === 0 || i === grid.length - 1 || j === grid.length - 1)
+      ) {
+        currentCell.isWall = true;
+      }
+    }
+  }
+};
+
+export const generateHorizontalMaze = (grid: CellInterface[][]) => {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      let currentCell = grid[i][j];
+      if (
+        !currentCell.isSnakeBodyPart &&
+        (i === 0 ||
+          j === 0 ||
+          i === grid.length - 1 ||
+          j === grid[0].length - 1 ||
+          ((i === 10 || i === 30) && j > 4 && j < 35))
+      ) {
+        currentCell.isWall = true;
+      }
+    }
+  }
+};
