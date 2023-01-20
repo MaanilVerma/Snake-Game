7c3c9c261d74295972f1a81aa9b84d0b884d0a55
Maanil Verma <vermaMaanil97@gmail.com>
2023-01-20 09:01:03 +0530

    feat: Add Cell Component with styles

---
+++ src/components/Cell.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { CellInterface } from "../interfaces";
+import { classNames } from "../utils";
+
+const Cell: React.FC<CellInterface & React.HTMLAttributes<HTMLDivElement>> = ({
+  isWall,
+  cellNumber,
+  column,
+  row,
+  isFood,
+  isSnakeBodyPart,
+  ...props
+}) => {
+  return (
+    <div
+      {...props}
+      className={classNames(
+        isWall ? "bg-gray-900 wall-animate" : "",
+        isFood ? "bg-red-500 rounded-full" : "",
+        isSnakeBodyPart ? "bg-green-600" : "",
+        "cell lg:w-4 w-3.5 lg:h-4 h-3.5 inline-flex justify-center items-center aspect-square border-[0.1px] border-indigo-300 border-opacity-70"
+      )}
+    ></div>
+  );
+};
